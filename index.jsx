7afd6c42ed4453f25b1ce24f76ea068e55341914
Andreas Gerlach <andi@appelgriebsch.org>
2017-04-03 11:03:21 +0200

    extension loader, updated type declarations

--- index.jsx
+++ index.jsx
@@ -1,7 +1,9 @@
 // @flow
 
+import ExtensionLoader from './utils/ExtensionLoader'
 import Injector from './utils/Injector'
 
 exports.utils = {
+  extensionLoader: new ExtensionLoader(),
   injector: new Injector()
 }
---
+++ utils/ExtensionLoader.jsx
@@ -0,0 +1,22 @@
+// @flow
+
+import path from 'path'
+import uri from 'url'
+
+import type { IExtension } from 'electron-shell'
+
+class ExtensionLoader {
+
+  tryLoadExtension(extensionFolder: string, extensionName: string):?IExtension {
+    let extensionInfo: IExtension
+    try {
+      extensionInfo = require(path.join(extensionFolder, extensionName))
+    } catch (ex) {
+      console.log(ex)
+      extensionInfo = undefined
+    }
+    return extensionInfo
+  }
+}
+
+export default ExtensionLoader
